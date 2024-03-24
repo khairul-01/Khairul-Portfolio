7ee66923d2567987a7745fe082bf8e10a2386c6e
Khairul Alam <khairul431743@gmail.com>
2024-03-24 17:16:42 +0600

    modified profile photo

--- src/pages/Portfolio/TrainingAndCertification/TrainingAndCertification.jsx
+++ src/pages/Portfolio/TrainingAndCertification/TrainingAndCertification.jsx
@@ -26,10 +26,30 @@ const TrainingAndCertification = () => {
                 </div>
                 <div className="card">
                     <figure className="px-6 sm:px-10 pt-10 h-72 sm:h-96">
+                        <img src="https://i.ibb.co/Lzh5Sb6/image.png" alt="conference management photo" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body  text-left">
+                        <h2 className="card-title">Web Development (MERN Stack)</h2>
+                        <p>Programming Hero | June 2023 -  January 2024</p>
+                        <p>Date of Passing: January, 2024</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <figure className="px-6 sm:px-10 pt-10 h-72 sm:h-96">
                         <img src="https://i.ibb.co/f8gwYNw/ICT-Certificates-3.jpg" alt="conference management photo" className="rounded-xl" />
                     </figure>
                     <div className="card-body  text-left">
-                        <h2 className="card-title">Bangladesh Miyazaki ICT Engineering Training Program (B-MEET)</h2>
+                        <h2 className="card-title">Bangladesh Miyazaki ICT Engineering Training Program Plus (B-MEET Plus) 6th Batch</h2>
+                        <p>B-JET Center, North South University</p>
+                        <p>Duration: One Month, February 2024</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <figure className="px-6 sm:px-10 pt-10 h-72 sm:h-96">
+                        <img src="https://i.ibb.co/f8gwYNw/ICT-Certificates-3.jpg" alt="conference management photo" className="rounded-xl" />
+                    </figure>
+                    <div className="card-body  text-left">
+                        <h2 className="card-title">Bangladesh Miyazaki ICT Engineering Training Program (B-MEET) 2nd Batch</h2>
                         <p>B-JET Center, North South University</p>
                         <p>Duration: One Month, May 2023</p>
                     </div>
--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -20,13 +20,13 @@ const Navbar = () => {
             whileHover={{ scale: 1.002 }}
             whileTap={{ scale: 0.98 }}
         >
-            <div className="navbar bg-violet-100 mb-1 text-black">
+            <div className="navbar z-10 bg-violet-100 mb-1 text-black">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost sm:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
-                        <ul tabIndex={0} className="menu gap-2 menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu gap-2 menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52">
                             {NavLinks}
                         </ul>
                     </div>
